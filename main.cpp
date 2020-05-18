21423e7a8a1a926cf93df5748f6a9d436cbff674
PetterJ123 <petter.johansson@protonmail.com>
2020-05-18 20:23:51 +0200

    Managed to atleast extract data from file

--- main.cpp
+++ main.cpp
@@ -3,70 +3,66 @@
 #include <fstream>  // std::ifstream
 #include <string> // std::string
 #include <vector>   // std::vector std::vector::iterator
+#include <sstream>
 
 // Author: Petter Johansson
 
-class Node{
-    public:
-        std::string name;
-};
-
-class Edge {
-    public:
-        Node *entrypoints[2];
-        int weight;
-};
-
-class Graph {
-    public:
-        Edge *edges;
-        Node *nodes;
-};
-
 int main(int argc, char *argv[]){
-    // Take in test data file path
-    std::string file_path;
-    if(argc < 2)
+	// Take in test data file path
+	std::string file_path;
+	if(argc < 2)
 	{
 		std::cout << "Missing arguments" << std::endl;
 		return -1;
-	}else{
-        file_path = argv[1];
-    }
-    std::string data;
+	}
+	else
+	{
+		file_path = argv[1];
+	}
+	std::string data;
 
-    // open file
-    std::ifstream read_file_stream;
-    read_file_stream.open(file_path, std::ios::in);
+	// open file
+	std::ifstream fs;
+	fs.open(file_path, std::ios::in);
+	
+	std::string line = "";
+	std::vector<std::string> nodes;
+	std::pair<std::pair<std::string, std::string>, int> connections_weight;
+	std::stringstream ss;
+	std::string node1, node2;
+	int weight;
+	bool run_connections = false;
 
-    // read nodes until space is found
-    std::string line = "";
-    std::vector<std::string> nodes;
-    std::vector<std::string>::iterator it;
-    std::vector<std::string> connections_n_weight;
-    bool run_connections = false;
+	// read file contents and push back into vector
+	while(std::getline(fs, line))
+	{
+		// file_content.push_back(line);
+		if(line.empty() && !run_connections)
+		{
+			run_connections = true;
+		}
+		else if(run_connections)
+		{
+			// add current line to string stream
+			ss.str(line);
+			// extract two nodes and weight
+			ss >> node1 >> node2 >> weight;
+			std::cout << "node1: " << node1 << 
+						"\nnode2: " << node2 <<
+						"\nweight: " << weight << 
+						"\n";
+			ss.clear();
+			ss.seekg(0, std::ios::beg);
+			// push back/add into pair-obj connections_weight
+			
+		}
+		else
+		{
+			std::cout << "l2: " << line << "\n";
+			nodes.push_back(line);
+		}
+		
+	}
 
-    while(std::getline(read_file_stream, line)){
-        if(!line.empty() && run_connections != true){
-            nodes.push_back(line);
-        }
-        else if(line.empty()){
-            run_connections = true;
-            std::cout << "run_connections now true\n";
-        }
-        else if(run_connections){
-            connections_n_weight.push_back(line);
-        }
-    }
-
-    for(it=nodes.begin(); it!=nodes.end(); ++it){
-        std::cout << *it << "\n";
-    }
-    for(it=connections_n_weight.begin(); it!=connections_n_weight.end(); ++it){
-        std::cout << *it << "\n";
-    }
-
-    return 0;
+	return 0;
 }
-
-    //TODO: struct/class of a node; character, connections
