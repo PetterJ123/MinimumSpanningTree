d5a6681b3f0ccfe8b2d953b772c82342668a75f0
PetterJ123 <petter.johansson@protonmail.com>
2020-05-19 15:11:22 +0200

    Extracted data totally

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream> // std::cout
-#include <utility>  // std::pair
 #include <fstream>  // std::ifstream
 #include <string> // std::string
 #include <vector>   // std::vector std::vector::iterator
-#include <sstream>
+#include <sstream>	// std::stringstream
+#include <tuple>	// std::tuple
 
 // Author: Petter Johansson
 
 int main(int argc, char *argv[]){
+	
 	// Take in test data file path
 	std::string file_path;
 	if(argc < 2)
@@ -19,24 +20,21 @@ int main(int argc, char *argv[]){
 	{
 		file_path = argv[1];
 	}
-	std::string data;
-
-	// open file
+	typedef std::tuple<std::string, std::string, int> conn_tuple;
 	std::ifstream fs;
 	fs.open(file_path, std::ios::in);
-	
-	std::string line = "";
+
+	std::string line = "", node1, node2, data;
 	std::vector<std::string> nodes;
-	std::pair<std::pair<std::string, std::string>, int> connections_weight;
+	std::vector<conn_tuple> conn_list;
+	conn_tuple vert_weight;
 	std::stringstream ss;
-	std::string node1, node2;
 	int weight;
 	bool run_connections = false;
 
 	// read file contents and push back into vector
 	while(std::getline(fs, line))
 	{
-		// file_content.push_back(line);
 		if(line.empty() && !run_connections)
 		{
 			run_connections = true;
@@ -54,15 +52,16 @@ int main(int argc, char *argv[]){
 			ss.clear();
 			ss.seekg(0, std::ios::beg);
 			// push back/add into pair-obj connections_weight
-			
+			std::get<0>(vert_weight) = node1;
+			std::get<1>(vert_weight) = node2;
+			std::get<2>(vert_weight) = weight;
+			conn_list.push_back(vert_weight);
 		}
-		else
-		{
-			std::cout << "l2: " << line << "\n";
-			nodes.push_back(line);
-		}
-		
 	}
 
+	for(const auto& i : conn_list){
+		std::cout << std::get<0>(i) << " " << std::get<1>(i) << " " << std::get<2>(i) << "\n";
+	}
+	
 	return 0;
 }
