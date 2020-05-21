65cb9c49be7ca814040456cb2ecd57d6b68567ae
PetterJ123 <petter.johansson@protonmail.com>
2020-05-21 23:40:50 +0200

    Cleaned up the code and made better structure

--- func.h
+++ func.h
@@ -12,10 +12,10 @@
 
 struct fileData
 {
-    std::vector<std::string> nodes;
+    std::vector<std::string> nodes, outputNodes;
     std::unordered_map<std::string, int> nodeMap;
     std::unordered_map<int, std::string> invertedNodeMap;
-    std::vector<std::tuple<std::string, std::string, int>> connections;
+    std::vector<std::tuple<std::string, std::string, int>> connections, outputWeights;
 };
 
 std::vector<Edge> kruskal(std::vector<Edge> edges, int vertCount)
@@ -30,7 +30,6 @@ std::vector<Edge> kruskal(std::vector<Edge> edges, int vertCount)
     {
         Edge nextEdge = edges.back();
         edges.pop_back();
-
         int x = disjointSet.Find(nextEdge.source);
         int y = disjointSet.Find(nextEdge.destination);
 
@@ -75,6 +74,46 @@ fileData extractData(fileData fd, std::string fpath)
         }
     }
     
+    return fd;
+}
+
+void outputKruskalsData(std::fstream& fstream, fileData fd)
+{
+    fstream.open("Answer.txt", std::ios::out | std::ios::trunc);
+    int k=0, i=0;
+    while(k!=fd.nodes.size())
+    {
+        if(fd.nodes[k] == fd.outputNodes[i])
+        {
+            // output to file here
+            fstream << fd.outputNodes[i] << "\n";
+            k++;
+        }
+        else if(i == fd.outputNodes.size())
+        {
+            i=0;
+        }
+        i++;
+    }
+    fstream << "\n";
+
+    for(auto &a : fd.outputWeights)
+    {
+        fstream << std::get<0>(a) << "\t" << std::get<1>(a) << "\t" << std::get<2>(a) << "\n";
+    }
+}
+
+fileData mapIntToStr(fileData fd, std::vector<Edge>& spt)
+{
+    for(auto &s : spt)
+    {
+        auto search = fd.invertedNodeMap.find(s.source);
+        auto search2 = fd.invertedNodeMap.find(s.destination);
+        fd.outputWeights.push_back({search->second, search2->second, s.weight});
+        fd.outputNodes.push_back(search->second);
+        fd.outputNodes.push_back(search2->second);
+    }
+
     return fd;
 }
 
--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <algorithm>
 #include <string>
 #include <fstream>
-#include <sstream>
-#include <tuple>
 #include "Edge.h"
 #include "DisjointSet.h"
 #include "func.h"
@@ -27,138 +25,25 @@ int main(int argc, char *argv[])
     }
     filePath = argv[1];
 
-    typedef std::vector<std::tuple<std::string, std::string, int>> tupleVector;
-
     std::fstream fileStream;
-    std::stringstream ss;
     fileData fd;
     fd = extractData(fd, filePath);
 
-    std::vector<std::string>::iterator iter;
-    for(iter=fd.nodes.begin(); iter!=fd.nodes.end(); iter++){
-        std::cout << *iter << "\n";
+    std::vector<Edge> edges;
+    for(auto& c : fd.connections)
+    {
+        auto search = fd.nodeMap.find(std::get<0>(c));
+        auto search2 = fd.nodeMap.find(std::get<1>(c));
+        edges.push_back({(int)search->second, (int)search2->second, (int)std::get<2>(c)});
     }
-
-    // int weight, mapNum = 0;
-    // bool connectionsBit;
-    // std::string line, srcNode, dstNode;
-    // std::unordered_map<std::string, int> nodeMap;
-    // std::unordered_map<int, std::string> nodeNrMap;
-    // std::vector<std::string> nodes;
-    // tupleVector connections;
     
-    // while(std::getline(fileStream, line))
-    // {
-    //     if(line.empty() && !connectionsBit)
-    //     {
-    //         connectionsBit = true;
-    //     }
-    //     else if(connectionsBit)
-    //     {
-    //         ss.str(line);
-    //         ss >> srcNode >> dstNode >> weight;
-    //         ss.clear();
-    //         ss.seekg(0, std::ios::beg);
-    //         connections.push_back({srcNode, dstNode, weight});
-    //     }
-    //     else
-    //     {
-    //         nodeMap[line] = mapNum;
-    //         nodeNrMap[mapNum] = line;
-    //         nodes.push_back(line);
-    //         mapNum++;
-    //     }
-    // }
-    // // find and replace each character in connections with corresponding character value in nodemap
+    std::sort(edges.begin(), edges.end(), compare());
 
-    // std::vector<Edge> edges;
-    // // std::cout << "==== CONNECTIONS =====\n";
-    // for(auto& c : connections)
-    // {
-    //     auto search = nodeMap.find(std::get<0>(c));
-    //     auto search2 = nodeMap.find(std::get<1>(c));
-    //     edges.push_back({(int)search->second, (int)search2->second, (int)std::get<2>(c)});
-    // }
+    std::vector<Edge> st = kruskal(edges, fd.nodes.size());
     
-    // std::sort(edges.begin(), edges.end(), compare());
+    fd = mapIntToStr(fd, st);
 
-    // std::vector<Edge> st = kruskal(edges, nodes.size());
-    
-    // std::vector<std::tuple<std::string, std::string, int>> res;
-    // std::vector<std::string> graphNodes;
-    
-    // for(auto &s : st)
-    // {
-    //     auto search = nodeNrMap.find(s.source);
-    //     auto search2 = nodeNrMap.find(s.destination);
-    //     res.push_back({search->second, search2->second, s.weight});
-    //     graphNodes.push_back(search->second);
-    //     graphNodes.push_back(search2->second);
-    // }
-
-    // int k=0, i=0;
-    // std::vector<std::string> outputNodes;
-    // while(k!=nodes.size())
-    // {
-    //     if(nodes[k] == graphNodes[i])
-    //     {
-    //         outputNodes.push_back(graphNodes[i]);
-    //         k++;
-    //     }
-    //     else if(i == graphNodes.size())
-    //     {
-    //         i=0;
-    //     }
-    //     i++;
-    // }
-
-    // std::cout << "======NODES=====\n";
-    // std::vector<std::string>::iterator it;
-    // for(it=outputNodes.begin(); it!=outputNodes.end(); it++)
-    // {
-    //     std::cout << *it << "\n";
-    // }
-
-    // std::cout << "\n========CONNECTIONS AND WEIGHT=========\n";
-    // for(auto &a : res)
-    // {
-    //     std::cout << std::get<0>(a) << "\t" << std::get<1>(a) << "\t" << std::get<2>(a) << "\n";
-    // }
-    // output to file
+    outputKruskalsData(fileStream, fd);
 
     return 0;
 }
-
-// nodeMap.at()
-// std::cout << std::get<0>(tv) << " : " << std::get<1>(tv) << " : " << std::get<2>(tv) << "\n";
-// std::get<0>(tv) = x.first;
-// std::cout << x.first << " : " << x.second << "\n";
-
-// while(i<nodeMap.size()){
-//     // std::cout << nodeMap.at(nodes[i]) << " ";
-//     i++;
-// }
-// for(const auto &n : nodeMap){
-//     std::cout << "key: " << n.first << " val: " << n.second << "\n";
-// }
-
-// for(auto& g:conn){
-//         std::cout << std::get<0>(g) << " " << std::get<1>(g) << " " << std::get<2>(g) << "\n";
-//     }
-
-// std::cout << "===== NODES =====\n";
-    // std::vector<std::string>::iterator it;
-    // for(it=nodes.begin(); it!= nodes.end(); it++){
-    //     std::cout << *it << "\n";
-    // }
-
-    // std::cout << "======= nodemap ======\n";
-    // // nodemap
-    // for(auto &nm : nodeMap){
-    //     std::cout << nm.first << " : " << nm.second << "\n";
-    // }
-    
-    // auto search = nodeMap.find("A");
-    // if(search != nodeMap.end()){
-    //     std::cout << search->first << " " << search->second << "\n";
-    // }else { std::cout << "nope\n"; }
